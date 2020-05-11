a2e4bb64aae9245345f643e0502de54171d8045b
josephbenjamin <josephbenjamin7@gmail.com>
2020-05-11 01:13:42 +0100

    Fix pair string in shortenHand
    also:
        - tidied up main.py
        - included *.pyc files in .gitignore
        - replaced the overwritten AA value in the strategy grids with 1 again

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ from blocks import *
 # clear the terminal window
 os.system('clear')
 
-# instantiate and then play a session
-#session = Session(handcount=5)
-#session.play()
-# for i in range(1,5):
-#     sitch = Situation()
-#     sitch.getDecision()
-#     sitch.evaluateDecision()
-#     if sitch.decision == "quit":
-#         break
-#     else: sitch.logSituation()
-sesh = Session(handcount=2, positionList=["UTG", "MID"], typeList=["OR"], shufflepos=False,shuffletype=False)
+sesh = Session(handcount=25, positionList=["UTG", "MID"], typeList=["OR"], shufflepos=True,shuffletype=False)
 sesh.runSession()
 sesh.summariseSession()
-
 
 """
 TODO
 
 A. prepare more strategy_grids
+        OR  ROL 3Bet
+    UTG  x    x    _
+    MID  x    x    _
+    CO   _    _    _
+    BTN  _    _    _
+    SB   _    _    _
+    BB   _    _    _
 B. create unit tests
-C.
+    - check all grids are present
+    - check all grids contain only 1 or 0
+C. difficult mode
+    i) focus on hardest hands only
+    ii) only test differences between positiosn passed
 
 """
